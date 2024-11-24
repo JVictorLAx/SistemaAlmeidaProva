5d12d3ac29d5140d12b921bc1f3ab79a0ee0e710
JVictorLAx <joao.almeida12@estudante.ifms.edu.br>
2024-11-24 14:31:11 -0400

    Primeiro Commit com todos os pacotes, telas e util

---
+++ src/tools/Util.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tools;
+
+import javax.swing.JComponent;
+
+/**
+ *
+ * @author u06296329105
+ */
+public class Util {
+
+    public static void habilitar(boolean valor, JComponent... comp) {
+        for (int i = 0; i < comp.length; i++) {
+            comp[i].setEnabled(valor);
+            
+        }
+    }
+
+}
